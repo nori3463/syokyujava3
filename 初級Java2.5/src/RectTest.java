a4a917bc76e3153525e14dc70ce2777241f7c2f5
hondahayato <hayato810honda@gmail.com>
2024-05-14 12:10:36 +0900

    3-3

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,10 +1,20 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30, 40, 50, 100);
+		
+		setColor(110,0,110);
 		fillRect(130, 40, 50, 100);
-		fillRect(30, 200, 150, 10);
-		fillRect(90, 160, 30, 30);
+		
+		setColor(0,255,255);
+		fillRect(230, 40, 50, 100);
+		
+		setColor(25,25,25);
+		fillRect(330, 40, 50, 100);
+		
+		setColor(110,110,110);
+		fillRect(20, 200, 150, 100);
 	}
 
 }
