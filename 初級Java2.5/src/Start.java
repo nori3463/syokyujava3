e1f50f52067b4f513c9dbe084cc109b4396ff1c8
hondahayato <hayato810honda@gmail.com>
2024-05-14 10:46:38 +0900

    3-1

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,17 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	
-		int x = 2;
-		int y = 3;
-		
-		System.out.println(x * y);
-		
-
-		MyFrame frame3 = new RectFrame();
-		frame3.setLocation(350, 550);
-		MyFrame frame4 = new RectFrame();
-		frame4.setLocation(550, 350);
+	System.out.println("プログラムを実行します");
+	MyFrame frame1=new RectTest();
 
 	}
 
