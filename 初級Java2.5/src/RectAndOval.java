9fb740274375bdb6a2fa0e7a5131352fb92a8506
hondahayato <hayato810honda@gmail.com>
2024-05-20 10:19:59 +0900

    練習問題3-1

---
+++ 初級Java2.5/src/RectAndOval.java
@@ -0,0 +1,19 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		setColor(43,39,9);
+		fillRect(70, 180, 100, 20);
+		
+		setColor(43,39,9);
+		fillRect(103, 165, 35, 30);
+		
+		setColor(43,39,9);
+		fillOval(80, 200, 20, 20);
+		
+		setColor(43,39,9);
+		fillOval(135, 200, 20, 20);
+
+		
+	}
+	}
+	
