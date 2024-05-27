39f76a4a9e37747356cd1c21863ab8f190af1a79
hondahayato <hayato810honda@gmail.com>
2024-05-27 09:53:37 +0900

    練習問題5-2

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,13 +2,14 @@
 public class Animation extends MyFrame{
 	public void run() {
 		int x=30;
+		
 		while (x<=170) {
 			
 			
 			clear();
 			
 			setColor(0,128,0);
-			fillRect(x, 80, 10, 100);
+			fillOval(x, 80, 100, 100);
 			x+=5;
 			sleep(0.1);
 		}
