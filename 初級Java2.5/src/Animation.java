bbc73214fa6512ffcbdae74de14010b1eec6bdcb
hondahayato <hayato810honda@gmail.com>
2024-05-27 10:47:08 +0900

    5-3

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,17 +1,28 @@
 
 public class Animation extends MyFrame{
 	public void run() {
+		int y=100;
 		int x=30;
-		while (x<=170) {
+		while (true) {
+			while(x<=170) {
 			
 			
 			clear();
 			
 			setColor(0,128,0);
-			fillRect(x, 80, 10, 100);
+			fillRect(x, y, 10, 100);
 			x+=5;
 			sleep(0.1);
 		}
+		while (y>=30) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x, y, 10, 100);
+			x-=5;
+			sleep(0.1);
+			
+		}
 	}
 
+}
 }
